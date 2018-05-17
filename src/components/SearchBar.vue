aead5db44fe6db5a5c9eaeaa4e8eb908fbf05d77
ADRIAN-ALIENWAR\abens <adrian.benson@snhu.edu>
2018-05-17 07:42:17 -0400

    deleted vue boilerplate. initial commit of app

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <input @input="onInput" type="text" />
+    </div>
+</template>
+
+<script>
+  export default {
+    name: "SearchBar",
+    methods: {
+      onInput: function(event) {
+        console.log(event.target.value);
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
