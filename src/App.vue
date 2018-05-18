40e4c9e4dcb5484ad6d344dcba7bc8b87f0a2a92
ADRIAN-ALIENWAR\abens <adrian.benson@snhu.edu>
2018-05-17 21:29:20 -0400

    added YouTube data api and search functionality

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,41 @@
 <template>
     <div>
-        <SearchBar></SearchBar>
+        <SearchBar @termChange="onTermChange"></SearchBar>
+        <VideoList :videos="videos"></VideoList>
     </div>
 </template>
 
 <script>
+  import axios from "axios";
   import SearchBar from "./components/SearchBar";
+  import VideoList from "./components/VideoList";
+
+  const API_KEY = "AIzaSyDEunT6kgL7GFlk9IYMPxCDoGzazygq68k";
+  const youTubeEndpoint = "https://www.googleapis.com/youtube/v3/search";
 
   export default {
     name: "App",
-    components: {SearchBar}
+    components: {SearchBar, VideoList},
+    data() {
+      return {
+        videos: []
+      };
+    },
+    methods: {
+      onTermChange(searchTerm) {
+        axios.get(youTubeEndpoint, {
+          params: {
+            key: API_KEY,
+            type: "video",
+            part: "snippet",
+            q: searchTerm
+          }
+        })
+          .then(response => {
+            this.videos = response.data.items;
+          });
+      }
+    }
   }
 </script>
 
